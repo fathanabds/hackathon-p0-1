66d8b101c4889610febd3e021d7a4ca68e6236c1
fathanabds <abdulshodiqfathan@gmail.com>
2024-09-10 21:56:14 +0700

    menambahkan document.get untuk elemen input add product

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let products = [
   { name: 'Spotify', price: 55000, desc: 'Spotify adalah layanan musik digital yang memberi Anda akses ke jutaan lagu.' },
   { name: 'Netflix', price: 60000, desc: 'Tonton film & acara TV Netflix secara online atau streaming langsung ke smart TV, konsol game, PC, Mac, ponsel, tablet, dan lainnya.' },
 ];
+
+const namaProduct = document.getElementById('namaProduct');
+const hargaProduct = document.getElementById('hargaProduct');
+const descProduct = document.getElementById('descProduct');
+const addProductBtn = document.getElementById('addProductBtn');

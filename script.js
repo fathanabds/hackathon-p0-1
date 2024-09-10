17bc0352ffba3627c29b37fdaf7823c866fa0678
fathanabds <abdulshodiqfathan@gmail.com>
2024-09-10 21:42:39 +0700

    menambahkan komentar guideline

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ let products = [
   { name: 'Spotify', price: 55000, desc: 'Spotify adalah layanan musik digital yang memberi Anda akses ke jutaan lagu.' },
   { name: 'Netflix', price: 60000, desc: 'Tonton film & acara TV Netflix secara online atau streaming langsung ke smart TV, konsol game, PC, Mac, ponsel, tablet, dan lainnya.' },
 ];
+
+// tulis script js sesuai dengan fitur masing-masing di bawah ini

4b9f72a1113e51bfbb6e3b8823ce914fa47ef577
fathanabds <abdulshodiqfathan@gmail.com>
2024-09-12 16:01:56 +0700

    add script tambah produk

--- script.js
+++ script.js
@@ -3,41 +3,6 @@ let products = [
   { name: 'Netflix', price: 60000, desc: 'Tonton film & acara TV Netflix secara online atau streaming langsung ke smart TV, konsol game, PC, Mac, ponsel, tablet, dan lainnya.' },
 ];
 
-// tulis script js sesuai dengan fitur masing-masing di bawah ini
-let productsList = document.getElementById('listProduct');
-products.forEach((product) => {
-  let makeDiv = document.createElement('div');
-  makeDiv.className = 'product';
-  makeDiv.innerHTML = `
-      <div class = imgHeader>
-        <img src = "img/a-simple-cloud-logo-or-icon-design-vector-removebg-preview (1).png"/>
-      </div>
-      <div class = "productHeader">
-          <label for = "proImg">${product.name}</label>
-
-      </div>
-      <div class = "harga">
-          <p>${new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(product.price)} /bulan</p>
-      </div>
-
-      <div class = "desc">
-          <p>${product.desc}</p>
-      </div>
-
-      <div class = "buttonDiList">
-          <button class="button-82-pushable" role="button">
-              <span class="button-82-shadow"></span>
-              <span class="button-82-edge"></span>
-              <span class="button-82-front text">
-                  Pesan
-              </span>
-          </button>
-      </div>
-      
-      `;
-  listProduct.appendChild(makeDiv);
-});
-
 // bagian add-product
 const namaProduct = document.getElementById('NamaProduk');
 const hargaProduct = document.getElementById('Harga');
@@ -46,12 +11,51 @@ const addProductBtn = document.getElementById('add-product-btn');
 
 addProductBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  products.push({
-    name: namaProduct.value,
-    price: Number(hargaProduct.value),
-    desc: descProduct.value,
-  });
-  products = products;
+  let newProduk = [
+    {
+      name: namaProduct.value,
+      price: Number(hargaProduct.value),
+      desc: descProduct.value,
+    },
+  ];
+  tampilProduk(newProduk);
 });
 
-console.log(products);
+// tulis script js sesuai dengan fitur masing-masing di bawah ini
+// let productsList = document.getElementById('listProduct');
+function tampilProduk(dataProduk) {
+  dataProduk.forEach((product) => {
+    let makeDiv = document.createElement('div');
+    makeDiv.className = 'product';
+    makeDiv.innerHTML = `
+				<div class = imgHeader>
+					<img src = "img/a-simple-cloud-logo-or-icon-design-vector-removebg-preview (1).png"/>
+				</div>
+				<div class = "productHeader">
+						<label for = "proImg">${product.name}</label>
+	
+				</div>
+				<div class = "harga">
+						<p>${new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(product.price)} /bulan</p>
+				</div>
+	
+				<div class = "desc">
+						<p>${product.desc}</p>
+				</div>
+	
+				<div class = "buttonDiList">
+						<button class="button-82-pushable" role="button">
+								<span class="button-82-shadow"></span>
+								<span class="button-82-edge"></span>
+								<span class="button-82-front text">
+										Pesan
+								</span>
+						</button>
+				</div>
+				
+				`;
+    listProduct.appendChild(makeDiv);
+  });
+}
+
+tampilProduk(products);
